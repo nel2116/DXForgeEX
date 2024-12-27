ea5405875a9c51a66a141a4fa726d11925ce584f
nel2116 <127926096+nel2116@users.noreply.github.com>
2024-12-27 16:09:57 +0900

    [add] DirectX12の初期化 [bugfix] Build時の構成がおかしくなっていたのを修正

--- DXForgeEngine/Graphics/Renderer.cpp
+++ DXForgeEngine/Graphics/Renderer.cpp
@@ -42,7 +42,7 @@ namespace dxforge::graphics
 
 	bool initialize(graphics_platform platform)
 	{
-		return set_platform_interface(platform);
+		return set_platform_interface(platform) && gfx.initialize();
 	}
 
 	void shutdown()
